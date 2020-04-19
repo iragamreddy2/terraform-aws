cd1685fe65eb8619340b500dfef13892fe79dd72
iragamreddy2 <iragamreddy2@gmail.com>
2020-04-19 13:49:17 -0500

    added variables and output tf files

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "aws_security_group" "instance" {
     name = "terraform_aws_example"
 
     ingress{
-        from_port = 8080
-        to_port = 8080
+        from_port = var.server_port
+        to_port = var.server_port
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
@@ -21,7 +21,7 @@ resource "aws_instance" "terraform_example" {
     user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, Welcome to terraform_aws_example" > index.html
-                nohup busybox httpd -f -p 8080 &
+                nohup busybox httpd -f -p "{var.server_port}" &
                 EOF
 
     tags = {
